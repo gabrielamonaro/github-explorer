ee83a2a0ae24c9d2d175a07e3e085db81c850e7d
gabrielamonaro <gabriela.monaro@aluno.ufabc.edu.brgit config --global user.email gabriela.monaro@aluno.ufabc.edu.br>
2023-07-11 22:40:54 -0300

    Primeira página com integração a API do GitHub

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,109 @@
+
+import React, {useState, FormEvent ,useEffect} from "react"
+import { Title, Form, Repositories, Error} from "./style"
+import logoImg from '../../assets/github-mark.svg'
+import { FiChevronRight} from 'react-icons/fi'
+
+import api from '../../services/api'
+import Repository from "../repository"
+
+interface Repository
+{
+//nao precisamos colocar a tipagem de tudo o que é retornado
+//precisamos colocar somente o que vamos usar!
+full_name: string
+description: string
+owner: {
+    login: string
+    avatar_url: string
+    }
+}
+const Dashboard: React.FC = () => {
+
+    const [newRepo, setNewRepo] = useState('')
+    const [inputError, setInputError] = useState('')
+    const [repositories, setRepositories] = useState<Repository[]>(() => {
+        const storagedRepositories = localStorage.getItem('@GithubExplorer:repositories')
+
+        if (storagedRepositories) {
+            return JSON.parse(storagedRepositories)
+        }
+        else {
+            return []
+        }
+    })
+
+
+    useEffect(()=> {
+        localStorage.setItem('@GithubExplorer:repositories', JSON.stringify(repositories))},  [repositories])
+
+    async function handleAddRepository (
+        event: FormEvent<HTMLFormElement>
+        ): Promise<void>
+    {
+        event.preventDefault()
+
+        if(!newRepo){
+            setInputError('Digite o autor/nome do repositório')
+            return
+        } 
+        
+        try{
+            //consumir a API do github
+        const response = await api.get(`repos/${newRepo}`)
+        console.log(response.data)
+        
+        //salvar no array dos repositorios
+        const repository = response.data
+        setRepositories([...repositories, repository])
+
+        //limando o input depois do submi
+        setNewRepo('')
+        setInputError('')
+        }
+
+        catch(err){
+            setInputError('Erro na busca por esse repositório.')
+        } 
+    }
+
+    return (
+        <>
+            <img src={logoImg} alt= "Github Explorer"/>
+            <Title>Explore repositórios no Github</Title>
+            
+
+            <Form 
+                haserror={!!inputError} 
+                onSubmit={handleAddRepository}
+            >
+                <input 
+                    value = {newRepo}
+                    onChange={e => setNewRepo(e.target.value)}
+                    placeholder="Digite o nome do repositório"
+                />
+                <button type ="submit"> Pesquisar</button>
+            </Form>
+
+        {inputError &&  <Error>{inputError}</Error> }
+
+            <Repositories>
+               {repositories.map(repository => (
+                    <a href="teste" key={repository.full_name}>
+                        <img src={repository.owner.avatar_url} 
+                        alt={repository.full_name} />
+                        <div>
+                            <strong> {repository.full_name} </strong>
+                            <p> {repository.description} </p>
+                        </div>
+                        <FiChevronRight size={20}/>
+                    </a>
+
+               ))}
+                
+            </Repositories>
+        </> 
+    )
+}
+
+export default Dashboard
